f6a9688860cdb19cc12aba00f60cc88fe02856cc
Rayzen-dev <rayzen.dev@gmail.com>
2020-08-23 13:42:44 +0200

    Ajout du pre-commit et ajout du trait Timestamp

---
+++ src/Helper/TimestampTrait.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Trait for Timestampable entities
+ *
+ * @package   App\Helper
+ * @version   1.0.0
+ * @author    Rayzen-dev <rayzen.dev@gmail.com>
+ * @copyright no copyrights
+ */
+
+namespace App\Helper;
+
+/**
+ * Trait TimestampTrait
+ */
+trait TimestampTrait
+{
+
+}
